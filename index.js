fb3be7bc5ae7a97ab79d76ae98418fd2da109c24
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-03-16 01:00:31 +0600

    forEach function called

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const displayQuiz = (data) => {
     return;
   }
 
-  data.forEach((quiz, i) => {
+  data.forEach = ((quiz, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
